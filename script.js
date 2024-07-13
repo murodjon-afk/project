ab45b6f09a2617f7e60d9547829d18da9fb85dca
murodjon72 <muro21601@gmail.com>
2024-07-13 11:34:30 +0500

    some updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr  = [4, 16, 19, 22, 11, 144, 967, 19124]
 let odd = []
 let even = arr.splice(0,1);
-let eve = arr.splice(1, 1);
+let eve = arr.splice(0, 1);
 let ev = arr.splice(1,1);
 let e = arr.splice(2,1);
 let l = arr.splice(3,1);
